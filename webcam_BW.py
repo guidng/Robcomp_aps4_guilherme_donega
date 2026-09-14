36a1d347f658e6ad5bb9e4e4a4fe058eb5f3ab7a
guilhermesd3 <guiguidonega@gmail.com>
2026-09-14 19:32:43 -0300

    webcam_BW.py

--- webcam_BW.py
+++ webcam_BW.py
@@ -1,16 +1,17 @@
 import cv2 as c
-class ProcessImage:
+import numpy as np
+class ProcessImage():
     def __init__(self):
         self.bgr = None
 
     def run_image(self, image):
         self.bgr = image
-        B,G,R = c.split(self.bgr)
-        self.bgr = c.merge([R,G,B])
-        self.bgr = c.transpose(self.bgr)
+        self.gray = c.cvtColor(self.bgr, c.COLOR_BGR2GRAY)
+        self.gray[self.gray >= 128] = 255
+        self.gray[self.gray < 128] = 0
 
     def show_image(self):
-        c.imshow("Imagem transposta", self.bgr)
+        c.imshow("imagem", self.gray)
         c.waitKey(1)
         
 
@@ -22,6 +23,12 @@ def main():
         imagem.run_image(image)
         imagem.show_image()
 
+
+    
     
 if __name__ == "__main__":
     main()   
+
+
+
+
