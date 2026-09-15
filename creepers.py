54d696d965bf300baac9502d995d57e9ca5031eb
guilhermesd3 <guiguidonega@gmail.com>
2026-09-15 17:44:28 -0300

    commit final

--- creepers.py
+++ creepers.py
@@ -11,17 +11,17 @@ class ProcessImage:
         self.hsv = c.cvtColor(self.bgr, c.COLOR_BGR2HSV)
 
         #Verde
-        g_low = np.array([35, 50, 50])
+        g_low = np.array([35, 150, 50])
         g_high = np.array([85, 255, 255])
         self.mask_green = c.inRange(self.hsv, g_low, g_high)
 
         #Vermelho
-        r_low = np.array([0, 50, 50])
+        r_low = np.array([0, 200, 100])
         r_high = np.array([10, 255, 255])
         self.mask_red = c.inRange(self.hsv, r_low, r_high)
 
         #Azul
-        b_low = np.array([90, 50, 50])
+        b_low = np.array([90, 150, 100])
         b_high = np.array([140, 255, 255])
         self.mask_blue = c.inRange(self.hsv, b_low, b_high)
 
